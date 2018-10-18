0676913b4fb26887113c0add552b92a75a527dbe
sendmebiscuits <Thomas Lee>
2018-10-18 16:21:20 -0400

    Add some test styling

--- src/Components/Presentational/App.js
+++ src/Components/Presentational/App.js
@@ -5,21 +5,6 @@ import GraphTypeSelector from './GraphTypeSelector';
 import GraphContainer from '../Container/GraphContainer';
 import MenuContainer from '../Container/MenuContainer';
 
-const style = {
-	app: {
-		display: 'flex',
-		height: '98vh',
-		alignItems: 'center',
-		flexDirection: 'column',
-		justifyContent: 'space-between'
-	},
-	menu: {
-		display: 'flex',
-		width: '35%',
-		justifyContent: 'space-between'
-	}
-};
-
 /**
  * To Add:
  * -	Zoomable y axis? Scatter scalable x?
@@ -28,13 +13,17 @@ const style = {
 const App = ({ match }) => {
 	const type = match.params.type || 'BAR_GRAPH';
 	return (
-		<div style={style.app}>
+		<div>
 			<Info type={type} />
-			<GraphContainer style={style.graphContainer} type={type} />
-			<div style={style.menu}>
-				<GraphTypeSelector />
-				<MenuContainer type={type} />
-			</div>
+			<GraphContainer type={type} />
+			<ul id="menu">
+				<li>
+					<GraphTypeSelector />
+				</li>
+				<li>
+					<MenuContainer type={type} />
+				</li>
+			</ul>
 		</div>
 	);
 };
