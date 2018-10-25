6b2da073825c817a027a05f05a0d0e77c9afc38c
sendmebiscuits <Thomas Lee>
2018-10-25 15:47:01 -0400

    Added sample api

--- lib/devserver/server.py
+++ lib/devserver/server.py
@@ -1,3 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, make_response, request, render_template, jsonify
+from flask_restful import Resource, Api, reqparse
+import random
 
-print('from python xD')
+app = Flask(__name__)
+api = Api(app)
+
+parser = reqparse.RequestParser()
+
+
+class sampleApi(Resource):
+    def get(self):
+        return jsonify({1: 'x'})
+
+
+api.add_resource(sampleApi, '/')
+
+
+if __name__ == '__main__':
+    app.run()
